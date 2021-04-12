e488d20adf1519b3a28723fc793ec49552524802
jeanoza <choikb916@gmail.com>
2021-04-12 22:06:12 +0200

    #1-2 Fetch Data & #1-3 Display with Google Chart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,69 @@
-import "./App.css";
+import { Chart } from "react-google-charts";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const api = axios.create({
+  baseURL: "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY",
+});
 
 function App() {
-  return <>Home</>;
+  const [neoDataArray, setNeoDataArray] = useState([]);
+  useEffect(() => {
+    api.get().then((response) => {
+      const {
+        data: { near_earth_objects },
+      } = response;
+      setNeoDataArray(near_earth_objects);
+    });
+  }, []);
+
+  return (
+    <>
+      <div style={{ display: "flex", maxWidth: 900 }}>
+        <Chart
+          width={900}
+          height={1200}
+          chartType="BarChart"
+          loader={<div>Loading Chart</div>}
+          data={[
+            [
+              "NEO Name",
+              "Min Estimated Diameter(km)",
+              "Max Estimated Diameter",
+            ],
+            ...neoDataArray
+              .sort(
+                (a, b) =>
+                  b.estimated_diameter.kilometers.estimated_diameter_min -
+                  a.estimated_diameter.kilometers.estimated_diameter_min
+              )
+              .map((neoData) => {
+                const id = neoData.id;
+                const name = neoData.name;
+                const max =
+                  neoData.estimated_diameter.kilometers.estimated_diameter_max;
+                const min =
+                  neoData.estimated_diameter.kilometers.estimated_diameter_min;
+
+                return [`${id} ${name.slice(name.indexOf("("))}`, min, max];
+              }),
+          ]}
+          options={{
+            chartArea: { width: "50%" },
+            hAxis: {
+              title: "Min Estimated Diameter(km)",
+              minValue: 0,
+            },
+            vAxis: {
+              title: "NEO Name",
+            },
+            legend: { position: "top" },
+          }}
+          legendToggle
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
